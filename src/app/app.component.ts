be3bf5a4413b67bc6d7000cdd95b5ff318db1e63
Sanket Gautam <sanketgautam08@gmail.com>
2022-07-14 14:25:15 +0545

    Testing ngIf Feature for dynamic DOM rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-beginner-to-advanced';
+  pressed: boolean = false;
+
+  onButtonClick() {
+    if (this.pressed === true) {
+      this.pressed = false;
+    } else {
+      this.pressed = true;
+    }
+    console.log(this.pressed);
+  }
 }
